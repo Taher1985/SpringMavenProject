12131bc704623c73d88e3da517aa36065dfaa1b2
Taher1985 <taherc.ali@gmail.com>
2018-03-25 10:48:08 +0530

    Compile Error

--- src/main/java/com/main/Beans.java
+++ src/main/java/com/main/Beans.java
@@ -10,7 +10,7 @@ public class Beans {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-beans-context.xml");
 		
-		Employee employee = (Employee) applicationContext.getBean("employee");
+		Employees employee = (Employee) applicationContext.getBean("employee");
 		employee.employeeCompanyName();
 	}
 
